6031e85a3ac96860b08379acf345ade16e6317e6
fibeesoft <fibeesoft@gmail.com>
2020-01-20 22:19:19 +0100

    first push

---
+++ Drone Simulator/Assets/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform playerTransform;
+    Vector3 playerPosition;
+    Vector3 cameraPosition;
+ 
+     void Update()
+    {
+        playerPosition = playerTransform.position;
+        cameraPosition = new Vector3(playerPosition.x, playerPosition.y + 1f, playerPosition.z - 5f);
+    }
+     void FixedUpdate()
+    {
+        FollowWithLerp();
+    }
+ 
+    void Follow(){
+        transform.position = cameraPosition;
+    }
+ 
+    void FollowWithLerp(){
+ 
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, 0.1f);
+    }
+}
---
+++ Drone Simulator/Assets/DroneMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    float movex, movey, moveForward;
+    float speedUp = 12f;
+    float speedX = 10f;
+    float speedForward = 10f;
+    float tiltAngle = 25f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        movex = Input.GetAxis("HorizontalTurn");
+        moveForward = Input.GetAxis("VerticalTurn");
+        movey = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector3(movex * speedX, movey * speedUp, moveForward * speedForward);
+        rb.rotation = Quaternion.Euler(tiltAngle * moveForward, 0f, -tiltAngle * movex);
+    }
+}
